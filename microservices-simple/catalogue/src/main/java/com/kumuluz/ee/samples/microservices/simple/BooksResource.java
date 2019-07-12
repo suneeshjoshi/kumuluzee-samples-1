c368f890018d2495b5bcd6a76ba1665a52e96a5b
Suneesh Joshi <suneesh.joshi@gmail.com>
2019-07-12 11:17:18 +0100

    Updating BookResource.java to add logic to add book when none is given for testing & adding index.html files

--- microservices-simple/catalogue/src/main/java/com/kumuluz/ee/samples/microservices/simple/BooksResource.java
+++ microservices-simple/catalogue/src/main/java/com/kumuluz/ee/samples/microservices/simple/BooksResource.java
@@ -20,16 +20,22 @@
 */
 package com.kumuluz.ee.samples.microservices.simple;
 
-import com.kumuluz.ee.samples.microservices.simple.models.Book;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+
+import com.kumuluz.ee.samples.microservices.simple.models.Book;
 
 /**
  * @author Tilen Faganel
@@ -41,59 +47,74 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 public class BooksResource {
 
-    @PersistenceContext
-    private EntityManager em;
+	@PersistenceContext
+	private EntityManager em;
 
-    /**
-     * <p>Queries the database and returns a list of all books.</p>
-     *
-     * @return Response object containing the retrieved list of books from the database.
-     */
-    @GET
-    public Response getBooks() {
+	/**
+	 * <p>
+	 * Queries the database and returns a list of all books.
+	 * </p>
+	 *
+	 * @return Response object containing the retrieved list of books from the
+	 *         database.
+	 */
+	@GET
+	public Response getBooks() {
 
-        TypedQuery<Book> query = em.createNamedQuery("Book.findAll", Book.class);
+		TypedQuery<Book> query = em.createNamedQuery("Book.findAll", Book.class);
 
-        List<Book> books = query.getResultList();
+		List<Book> books = query.getResultList();
 
-        return Response.ok(books).build();
-    }
+		return Response.ok(books).build();
+	}
 
-    /**
-     * <p>Queries the database and returns a specific book based on the given id.</p>
-     *
-     * @param id The id of the wanted book.
-     * @return Response object containing the requested book, or empty with the NOT_FOUND status.
-     */
-    @GET
-    @Path("/{id}")
-    public Response getBook(@PathParam("id") Integer id) {
+	/**
+	 * <p>
+	 * Queries the database and returns a specific book based on the given id.
+	 * </p>
+	 *
+	 * @param id The id of the wanted book.
+	 * @return Response object containing the requested book, or empty with the
+	 *         NOT_FOUND status.
+	 */
+	@GET
+	@Path("/{id}")
+	public Response getBook(@PathParam("id") Integer id) {
 
-        Book b = em.find(Book.class, id);
+		Book b = em.find(Book.class, id);
 
-        if (b == null)
-            return Response.status(Response.Status.NOT_FOUND).build();
+		if (b == null)
+			return Response.status(Response.Status.NOT_FOUND).build();
 
-        return Response.ok(b).build();
-    }
+		return Response.ok(b).build();
+	}
 
-    /**
-     * <p>Inserts the provided book into the database.</p>
-     *
-     * @param b The book object which will be created.
-     * @return Response object containing the created book.
-     */
-    @POST
-    public Response createBook(Book b) {
+	/**
+	 * <p>
+	 * Inserts the provided book into the database.
+	 * </p>
+	 *
+	 * @param b The book object which will be created.
+	 * @return Response object containing the created book.
+	 */
+	@POST
+	public Response createBook(Book b) {
+		Book toUse;
 
-        b.setId(null);
+		if (b != null) {
+			toUse = b;
+		} else {
+			toUse = new Book();
+		}
 
-        em.getTransaction().begin();
+		toUse.setId(null);
 
-        em.persist(b);
+		em.getTransaction().begin();
 
-        em.getTransaction().commit();
+		em.persist(toUse);
 
-        return Response.status(Response.Status.CREATED).entity(b).build();
-    }
+		em.getTransaction().commit();
+		return Response.status(Response.Status.CREATED).entity(toUse).build();
+	}
+
 }

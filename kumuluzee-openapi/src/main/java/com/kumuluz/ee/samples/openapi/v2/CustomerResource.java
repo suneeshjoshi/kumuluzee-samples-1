3eee302e484bcd3fce7fdd0ac1995aab4f347aa0
Suneesh Joshi <suneesh.joshi@gmail.com>
2019-07-16 00:05:24 +0100

    Added extra Customer Objects to understand the OpenAPI ( formerly Swagger )

--- kumuluzee-openapi/src/main/java/com/kumuluz/ee/samples/openapi/v2/CustomerResource.java
+++ kumuluzee-openapi/src/main/java/com/kumuluz/ee/samples/openapi/v2/CustomerResource.java
@@ -1,11 +1,7 @@
 package com.kumuluz.ee.samples.openapi.v2;
 
-import com.kumuluz.ee.samples.openapi.Customer;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,8 +9,14 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
+
+import com.kumuluz.ee.samples.openapi.Customer;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 /**
  * CustomerResource class with openAPI
@@ -26,38 +28,33 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class CustomerResource {
 
-    @GET
-    @Operation(summary = "Get customers list", tags = {"customers"}, description = "Returns a list of customers.",
-            responses = {
-                    @ApiResponse(
-                            description = "List of customers",
-                            responseCode = "200",
-                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = Customer.class)))
-                    )
-            })
-    public Response getCustomers() {
+	@GET
+	@Operation(summary = "Get customers list", tags = {
+			"customers" }, description = "Returns a list of customers.", responses = {
+					@ApiResponse(description = "List of customers", responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Customer.class)))) })
+	public Response getCustomers() {
 
-        List<Customer> customers = new ArrayList<>();
-        Customer c = new Customer("1", "John", "Doe");
+		List<Customer> customers = new ArrayList<>();
+		Customer c = new Customer("1", "John", "Doe");
 
-        customers.add(c);
+		customers.add(c);
+		customers.add(new Customer("2", "Suneesh", "Joshi"));
+		customers.add(new Customer("3", "Charitha", "Joshi"));
+		customers.add(new Customer("4", "Muneesh", "Joshi"));
 
-        return Response.status(Response.Status.OK).entity(customers).build();
-    }
+		return Response.status(Response.Status.OK).entity(customers).build();
+	}
 
-    @GET
-    @Operation(summary = "Get customers details", tags = {"customers"}, description = "Returns customer details.",
-            responses = {
-                    @ApiResponse(description = "Customer details", responseCode = "200", content = @Content(schema =
-                    @Schema(implementation =
-                            Customer.class)))
-            })
-    @Path("{customerId}")
-    public Response getCustomer(@PathParam("customerId") String customerId) {
+	@GET
+	@Operation(summary = "Get customers details", tags = {
+			"customers" }, description = "Returns customer details.", responses = {
+					@ApiResponse(description = "Customer details", responseCode = "200", content = @Content(schema = @Schema(implementation = Customer.class))) })
+	@Path("{customerId}")
+	public Response getCustomer(@PathParam("customerId") String customerId) {
 
-        Customer c = new Customer("1", "John", "Doe");
+		Customer c = new Customer("1", "John", "Doe");
 
-        return Response.status(Response.Status.OK).entity(c).build();
-    }
+		return Response.status(Response.Status.OK).entity(c).build();
+	}
 
 }
